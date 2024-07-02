8c1bc1b9d7537cd57ca64a54a0c59a89f0c42d02
Ibrahem-Khaled <xx.vvv48@gmail.com>
2024-07-02 17:51:23 +0200

    hide rows

--- resources/views/FinancialManagement/Revenues/accountStatement.blade.php
+++ resources/views/FinancialManagement/Revenues/accountStatement.blade.php
@@ -1,26 +1,25 @@
 @extends('layouts.default')
 
 @section('content')
-
-    <div class="col-md-12">
-        <h1 class="text-success" style="text-align: right">كشف حساب {{ $user->name }}</h1>
+    <div class="col-md-12 text-right">
+        <h1 class="text-success">كشف حساب {{ $user->name }}</h1>
     </div>
 
     <div class="container mt-5">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">اجمالي سعر النقل</th>
-                    <th scope="col">سعر النقل</th>
-                    <th scope="col">سعر امر النقل</th>
-                    <th scope="col">عدد الحاويات</th>
-                    <th scope="col">العميل</th>
-                    <th scope="col">رقم البيان</th>
-                    <th scope="col">#</th>
-                </tr>
-            </thead>
-            <form action="{{ route('updateContainerPrice') }}" method="POST">
-                @csrf
+        <form action="{{ route('updateContainerPrice') }}" method="POST">
+            @csrf
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">اجمالي سعر النقل</th>
+                        <th scope="col">سعر النقل</th>
+                        <th scope="col">سعر امر النقل</th>
+                        <th scope="col">عدد الحاويات</th>
+                        <th scope="col">العميل</th>
+                        <th scope="col">رقم البيان</th>
+                        <th scope="col">#</th>
+                    </tr>
+                </thead>
                 <tbody>
                     @php
                         $totalPrice = 0;
@@ -39,9 +38,7 @@
                             <input type="hidden" value="{{ $custom->id }}" name="id[]" />
                             <tr>
                                 @php
-                                    $containerPrice = $custom->container
-                                        ->whereIn('status', ['transport', 'done', 'rent', 'wait'])
-                                        ->sum('price');
+                                    $containerPrice = $transportContainers->sum('price');
                                     $withdrawPrice = $custom->container
                                         ->flatMap(fn($c) => $c->daily)
                                         ->where('type', 'withdraw')
@@ -49,40 +46,34 @@
                                     $totalPrice += $containerPrice;
                                     $totalWithdrawPrice += $withdrawPrice;
                                 @endphp
-
                                 <td>{{ $containerPrice }}</td>
                                 <td>
                                     <div class="input-group mb-3">
                                         @if (auth()->user()?->userinfo?->job_title == 'administrative')
                                             <input type="text"
-                                                value="{{ $containerPrice / $custom->container->whereIn('status', ['transport', 'done', 'rent', 'wait'])->count() }}"
-                                                class="custom-form-control" placeholder="سعر الحاوية"
-                                                aria-label="سعر الحاوية" aria-describedby="basic-addon2" disabled>
+                                                value="{{ $containerPrice / $transportContainers->count() }}"
+                                                class="form-control" placeholder="سعر الحاوية" disabled>
                                         @else
                                             <input type="text" name="price[]"
-                                                value="{{ $containerPrice / $custom->container->whereIn('status', ['transport', 'done', 'rent', 'wait'])->count() }}"
-                                                class="custom-form-control" placeholder="سعر الحاوية"
-                                                aria-label="سعر الحاوية" aria-describedby="basic-addon2">
+                                                value="{{ $containerPrice / $transportContainers->count() }}"
+                                                class="form-control" placeholder="سعر الحاوية">
                                         @endif
                                     </div>
                                 </td>
                                 <td>{{ $withdrawPrice }}</td>
-                                <td>{{ $custom->container->whereIn('status', ['transport', 'done', 'rent', 'wait'])->count() }}
+                                <td>{{ $transportContainers->count() }}</td>
+                                <td>{{ $custom->subclient_id }}</td>
+                                <td>
+                                    <a href="{{ route('showContainer', $custom->id) }}">{{ $custom->statement_number }}</a>
                                 </td>
-                                <td scope="row">{{ $custom->subclient_id }}</td>
-                                <td scope="row">
-                                    <a href="{{ route('showContainer', $custom->id) }}">
-                                        {{ $custom->statement_number }}
-                                    </a>
-                                </td>
-                                <th scope="row">{{ $custom->id }}</th>
+                                <td>{{ $custom->id }}</td>
                             </tr>
                         @endif
                     @endforeach
                 </tbody>
-                <button type="submit" class="btn btn-primary">تاكيد سعر الحاوية</button>
-            </form>
-        </table>
+            </table>
+            <button type="submit" class="btn btn-primary">تاكيد سعر الحاوية</button>
+        </form>
 
         <table class="table mt-5">
             <thead>
@@ -94,46 +85,42 @@
             <tbody>
                 <tr>
                     <td>
-                        <h6 class="text-dark">{{ $totalPrice }}</h6>
+                        <h6 class="text-primary">مجموع سعر الحاويات</h6>
                     </td>
                     <td>
-                        <h6 class="text-primary">مجموع سعر الحاويات</h6>
+                        <h6 class="text-dark">{{ $totalPrice }}</h6>
                     </td>
                 </tr>
                 <tr>
-                    <td>
-                        <h6 class="text-dark">{{ $totalWithdrawPrice }}</h6>
-                    </td>
                     <td>
                         <h6 class="text-dark">مجموع اوامر النقل</h6>
                     </td>
+                    <td>
+                        <h6 class="text-dark">{{ $totalWithdrawPrice }}</h6>
+                    </td>
                 </tr>
                 <tr>
                     <td>
-                        <h6 class="text-dark">{{ $totalWithdrawPrice + $totalPrice }}</h6>
+                        <h6 class="text-dark">الاجمالي</h6>
                     </td>
                     <td>
-                        <h6 class="text-dark">الاجمالي</h6>
+                        <h6 class="text-dark">{{ $totalWithdrawPrice + $totalPrice }}</h6>
                     </td>
                 </tr>
                 <tr id="vatRow">
-                    <td>
-                        @php
-                            $sumWith = ($totalWithdrawPrice + $totalPrice) * 0.15;
-                        @endphp
-                        <h6 class="text-dark" id="vatValue">{{ $sumWith }}</h6>
-                    </td>
                     <td>
                         <h6 class="text-success">(% 15) القيمة المضافة</h6>
                     </td>
-                </tr>
-
-                <tr id="vatRow">
                     <td>
-                        <h6 class="text-dark" id="totalWithVat">{{ $totalWithdrawPrice + $totalPrice + $sumWith }}</h6>
+                        <h6 class="text-dark" id="vatValue">{{ ($totalWithdrawPrice + $totalPrice) * 0.15 }}</h6>
                     </td>
+                </tr>
+                <tr id="totalWithVatRow">
                     <td>
                         <h6 class="text-danger">الاجمالي شامل القيمة المضافة</h6>
+                    </td>
+                    <td>
+                        <h6 class="text-dark" id="totalWithVat">{{ ($totalWithdrawPrice + $totalPrice) * 1.15 }}</h6>
                     </td>
                 </tr>
             </tbody>
@@ -145,22 +132,23 @@
     <script>
         function toggleVat() {
             var vatRow = document.getElementById('vatRow');
-            var vatValue = document.getElementById('vatValue');
-            var totalWithVat = document.getElementById('totalWithVat');
+            var totalWithVatRow = document.getElementById('totalWithVatRow');
             var toggleButton = document.getElementById('toggleVatButton');
-            var totalWithoutVat = {{ $totalWithdrawPrice + $totalPrice }};
-            var vatAmount = {{ $sumWith }};
 
             if (vatRow.style.display === 'none') {
                 vatRow.style.display = 'table-row';
-                totalWithVat.textContent = totalWithoutVat + vatAmount;
+                totalWithVatRow.style.display = 'table-row';
                 toggleButton.textContent = 'اخفاء القيمة المضافة';
             } else {
                 vatRow.style.display = 'none';
-                totalWithVat.textContent = totalWithoutVat;
+                totalWithVatRow.style.display = 'none';
                 toggleButton.textContent = 'اظهار القيمة المضافة';
             }
         }
-    </script>
 
-@stop
+        document.addEventListener("DOMContentLoaded", function() {
+            var totalWithVatRow = document.getElementById('totalWithVatRow');
+            totalWithVatRow.style.display = 'none'; // إخفاء الإجمالي شامل القيمة المضافة عند تحميل الصفحة
+        });
+    </script>
+@endsection
